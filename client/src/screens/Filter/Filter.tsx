3829676568f392e86c1e15b60ca53d7c273bda41
Sasha Sushko <sushko@outlook.com>
2019-05-18 22:23:05 +0500

    Добавит черновой вариант главной страницы и настроек

--- client/src/screens/Filter/Filter.tsx
+++ client/src/screens/Filter/Filter.tsx
@@ -1,14 +1,163 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import querystring from "querystring";
 import routes from "../../routes";
+import { RouteComponentProps } from "react-router";
 
-interface FilterProps {}
+interface FilterProps extends RouteComponentProps {}
 
-export function Filter(props: FilterProps) {
-  return (
-    <div>
-      <Link to={routes.settings}>Настройки</Link> &middot;{" "}
-      <Link to={routes.result}>Найти</Link>
-    </div>
-  );
+export class Filter extends React.Component<FilterProps> {
+  state = {
+    drinkType: "0",
+    beerType: "0",
+    vineType: "0",
+    barsCount: "3"
+  };
+
+  parseData = () => {
+    const { drinkType, beerType, vineType, barsCount } = this.state;
+
+    const obj = {
+      drinkType,
+      barsCount
+    };
+
+    if (drinkType === "0") {
+      return {
+        ...obj,
+        beerType
+      };
+    }
+
+    if (drinkType === "1") {
+      return {
+        ...obj,
+        vineType
+      };
+    }
+  };
+
+  handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    const params = querystring.stringify(this.parseData());
+    this.props.history.push(`${routes.result}?${params}`);
+  };
+
+  handleChange = (
+    evt: React.FormEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    this.setState({
+      [evt.currentTarget.name]: evt.currentTarget.value
+    });
+  };
+
+  render() {
+    const { drinkType, beerType, vineType, barsCount } = this.state;
+
+    return (
+      <div>
+        <Link to={routes.settings}>Настройки</Link>
+
+        <form onSubmit={this.handleSubmit}>
+          <select
+            value={drinkType}
+            name="drinkType"
+            onChange={this.handleChange}
+          >
+            <option value="0">Пиво</option>
+            <option value="1">Вино</option>
+            <option value="2">Крепкий алкоголь</option>
+          </select>
+
+          {drinkType === "0" && (
+            <fieldset>
+              <legend>Пиво</legend>
+
+              <input
+                type="radio"
+                name="beerType"
+                id="beerType0"
+                value="0"
+                onChange={this.handleChange}
+                checked={beerType === "0"}
+              />
+              <label htmlFor="beerType0">Светлое</label>
+
+              <input
+                type="radio"
+                name="beerType"
+                id="beerType1"
+                value="1"
+                checked={beerType === "1"}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="beerType1">Тёмное</label>
+            </fieldset>
+          )}
+
+          {drinkType === "1" && (
+            <fieldset>
+              <legend>Вино</legend>
+
+              <input
+                type="radio"
+                name="vineType"
+                id="vineType0"
+                value="0"
+                onChange={this.handleChange}
+                checked={vineType === "0"}
+              />
+              <label htmlFor="vineType0">Белое</label>
+
+              <input
+                type="radio"
+                name="vineType"
+                id="vineType0"
+                value="1"
+                onChange={this.handleChange}
+                checked={vineType === "1"}
+              />
+              <label htmlFor="vineType0">Красная</label>
+            </fieldset>
+          )}
+
+          <fieldset>
+            <legend>Количество баров</legend>
+
+            <input
+              type="radio"
+              name="barsCount"
+              id="barsCount3"
+              value="3"
+              onChange={this.handleChange}
+              checked={barsCount === "3"}
+            />
+            <label htmlFor="barsCount3">3</label>
+
+            <input
+              type="radio"
+              name="barsCount"
+              id="barsCount5"
+              value="5"
+              onChange={this.handleChange}
+              checked={barsCount === "5"}
+            />
+            <label htmlFor="barsCount5">5</label>
+
+            <input
+              type="radio"
+              name="barsCount"
+              id="barsCount8"
+              value="8"
+              onChange={this.handleChange}
+              checked={barsCount === "8"}
+            />
+            <label htmlFor="barsCount8">8 баров</label>
+          </fieldset>
+
+          <button>Подобрать маршрут</button>
+        </form>
+      </div>
+    );
+  }
 }
--- client/src/screens/Settings/Settings.tsx
+++ client/src/screens/Settings/Settings.tsx
@@ -4,10 +4,88 @@ import routes from "../../routes";
 
 interface SettingsProps {}
 
-export function Settings(props: SettingsProps) {
-  return (
-    <div>
-      <Link to={routes.filter}>Назад</Link>
-    </div>
-  );
+export class Settings extends React.Component<SettingsProps> {
+  state = {
+    name: "",
+    weight: "",
+    height: "",
+    sex: "male"
+  };
+
+  handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+  };
+
+  handleChange = (
+    evt: React.FormEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    this.setState({
+      [evt.currentTarget.name]: evt.currentTarget.value
+    });
+  };
+
+  render() {
+    const { name, sex, weight, height } = this.state;
+
+    return (
+      <div>
+        <Link to={routes.filter}>Назад</Link>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label htmlFor="name">Имя</label>
+            <input
+              id="name"
+              type="text"
+              value={name}
+              name="name"
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <label htmlFor="weight">Масса</label>
+            <input
+              id="weight"
+              type="number"
+              value={weight}
+              name="weight"
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <label htmlFor="height">Рост</label>
+            <input
+              id="height"
+              type="number"
+              name="height"
+              value={height}
+              onChange={this.handleChange}
+            />
+          </p>
+          <fieldset>
+            <legend>Пол</legend>
+
+            <input
+              type="radio"
+              name="sex"
+              id="male"
+              value="male"
+              checked={sex === "male"}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="male">М</label>
+
+            <input
+              type="radio"
+              name="sex"
+              id="female"
+              value="female"
+              checked={sex === "female"}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="female">Ж</label>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
 }
