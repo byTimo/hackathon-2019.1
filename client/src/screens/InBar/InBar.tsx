7162e8095d7ed274e52376779e4e00c98ca88c04
Sasha Sushko <sushko@outlook.com>
2019-05-19 12:26:03 +0500

    Добавит экран в баре

--- client/src/screens/InBar/InBar.tsx
+++ client/src/screens/InBar/InBar.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import LocalBarIcon from "@material-ui/icons/LocalBar";
+import Typography from "@material-ui/core/Typography";
+import "./InBar.css";
+import routes from "../../routes";
+import { RouteComponentProps } from "react-router";
 
-interface InBarProps {}
+interface InBarProps extends RouteComponentProps {}
 
 export function InBar(props: InBarProps) {
-  return <div />
+  return (
+    <div>
+      <Typography variant="h2" align="center" className="time">
+        05:00:00
+      </Typography>
+      <ul className="alco-list">
+        <li>
+          <Button variant="contained" color="default">
+            <LocalBarIcon /> 0,3
+          </Button>
+        </li>
+        <li>
+          <Button variant="contained" color="default">
+            <LocalBarIcon /> 0,5
+          </Button>
+        </li>
+        <li>
+          <Button variant="contained" color="default">
+            <LocalBarIcon /> 1,0
+          </Button>
+        </li>
+      </ul>
+      <div className="go">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => props.history.push(routes.navigation)}
+        >
+          Погнали дальше
+        </Button>
+      </div>
+    </div>
+  );
 }
