ddc03b9cd7163ad2a23180cfe2460f34d5433965
Sasha Sushko <sushko@outlook.com>
2019-05-19 12:09:52 +0500

    Добавил кнопку перехода в бар

--- client/src/screens/Navigation/Navigation.tsx
+++ client/src/screens/Navigation/Navigation.tsx
@@ -1,59 +1,74 @@
-import React, {useRef} from "react";
-import {Placemark, ZoomControl} from "react-yandex-maps";
-import {Redirect} from "react-router";
+import React, { useRef } from "react";
+import { Placemark, ZoomControl } from "react-yandex-maps";
+import { Redirect, RouteComponentProps } from "react-router";
 
-import {useUserCoordinates} from "../../hooks/useUserCoordinates";
-import {useWindowSize} from "../../hooks/useWindowSize";
+import { useUserCoordinates } from "../../hooks/useUserCoordinates";
+import { useWindowSize } from "../../hooks/useWindowSize";
 
-import {useTrip} from "../../contexts/TripContext";
-import {TripMap} from "../../components/TripMap/TripMap";
+import { useTrip } from "../../contexts/TripContext";
+import { TripMap } from "../../components/TripMap/TripMap";
 
 import routes from "../../routes";
 
 import userIcon from "./user-icon.png";
-import {geoPositionToCoords} from "../../lib/geoPosition";
+import { geoPositionToCoords } from "../../lib/geoPosition";
 
-interface NavigationProps {
-}
+import { Button } from "@material-ui/core";
+
+interface NavigationProps extends RouteComponentProps {}
 
 export function Navigation(props: NavigationProps) {
-    const mapRef = useRef<any>(null);
-    const coords = useUserCoordinates();
-    const size = useWindowSize();
-    const {trip} = useTrip();
+  const mapRef = useRef<any>(null);
+  const coords = useUserCoordinates();
+  const size = useWindowSize();
+  const { trip } = useTrip();
 
-    function resize() {
-        const map = mapRef.current;
-        if (map) {
-            const bounds = map.geoObjects.getBounds();
-            if (bounds) map.setBounds(bounds);
-        }
+  function resize() {
+    const map = mapRef.current;
+    if (map) {
+      const bounds = map.geoObjects.getBounds();
+      if (bounds) map.setBounds(bounds);
     }
+  }
 
-    if (!coords) {
-        return <>Loading...</>;
-    }
+  if (!coords) {
+    return <>Loading...</>;
+  }
 
-    if (!trip || !trip.bars.length) {
-        return <Redirect to={routes.map.filter}/>;
-    }
+  if (!trip || !trip.bars.length) {
+    return <Redirect to={routes.map.filter} />;
+  }
 
-    return (
-        <TripMap
-            {...size}
-            //bars={trip.bars}
-            // mapRef={x => (mapRef.current = x)}
-        >
-            <ZoomControl/>
-            <Placemark
-                onLoad={resize}
-                geometry={geoPositionToCoords(coords)}
-                options={{
-                    iconLayout: "default#image",
-                    iconImageHref: userIcon,
-                    iconImageSize: [46, 46]
-                }}
-            />
-        </TripMap>
-    );
+  return (
+    <TripMap
+      {...size}
+      //bars={trip.bars}
+      // mapRef={x => (mapRef.current = x)}
+    >
+      <ZoomControl />
+      <Placemark
+        onLoad={resize}
+        geometry={geoPositionToCoords(coords)}
+        options={{
+          iconLayout: "default#image",
+          iconImageHref: userIcon,
+          iconImageSize: [46, 46]
+        }}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        style={{
+          position: "absolute",
+          zIndex: 100,
+          left: "50%",
+          bottom: 30,
+          transform: "translateX(-50%)"
+        }}
+        onClick={() => props.history.push(routes.inBar)}
+      >
+        Я пришёл. Напиться
+      </Button>
+    </TripMap>
+  );
 }
--- client/src/screens/Result/Trip.tsx
+++ client/src/screens/Result/Trip.tsx
@@ -1,42 +1,44 @@
 import * as React from "react";
-import {Redirect, RouteComponentProps, withRouter} from "react-router";
-import {TripContextValue, withTrip} from "../../contexts/TripContext";
-import {Panel} from "../../components/Panel";
+import { Redirect, RouteComponentProps, withRouter } from "react-router";
+import { TripContextValue, withTrip } from "../../contexts/TripContext";
+import { Panel } from "../../components/Panel";
 import routes from "../../routes";
 import Gapped from "../../components/Gapped";
-import {Button} from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 interface TripRouteParams {
-    barType: string;
-    barsCount: string;
+  barType: string;
+  barsCount: string;
 }
 
-export interface TripProps extends TripContextValue, RouteComponentProps<TripRouteParams>{
+export interface TripProps
+  extends TripContextValue,
+    RouteComponentProps<TripRouteParams> {}
 
-}
+class TripInternal extends React.Component<TripProps> {
+  render() {
+    if (!this.props.trip) return <Redirect to={routes.map.filter} />;
 
-class TripInternal extends React.Component<TripProps>{
-    render() {
-        if(!this.props.trip)
-            return <Redirect to={routes.map.filter}/>
-
-        return (
-            <Panel>
-                <Gapped vertical gap={5} alignItems={"center"}>
-                {this.props.trip.bars.map((x) => (
-                    <div key={x.id}>
-                        <span>{x.title}</span>
-                    </div>
-                ))}
-                </Gapped>
-                <Gapped>
-                    <Button onClick={() => this.props.history.push(routes.map.filter)}>
-                       Изменить
-                    </Button>
-                </Gapped>
-            </Panel>
-        )
-    }
+    return (
+      <Panel>
+        <Gapped vertical gap={5} alignItems={"center"}>
+          {this.props.trip.bars.map(x => (
+            <div key={x.id}>
+              <span>{x.title}</span>
+            </div>
+          ))}
+        </Gapped>
+        <Gapped>
+          <Button onClick={() => this.props.history.push(routes.map.filter)}>
+            Изменить
+          </Button>
+          <Button onClick={() => this.props.history.push(routes.navigation)}>
+            Погнали, народ
+          </Button>
+        </Gapped>
+      </Panel>
+    );
+  }
 }
 
 export const Trip = withRouter(withTrip(TripInternal));
